6f8b0b55e1e58c6afbef4a797dcdf04736db630d
Virlla Devi <virla_devi@yahoo.com>
2014-05-21 22:08:33 +0500

    myInit

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+   has_many :posts
+       has_attached_file :avatar
+      
+     validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]              
+end
